89efa87651b62adb51b613a290ef40e1a15de21d
Evgeny Novoselov <zkodru@gmail.com>
2020-04-21 19:03:08 +0300

    Создал необходимые классы

---
+++ chapter_001/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -0,0 +1,9 @@
+package ru.job4j.generic;
+
+/**
+ * Обертка над массивом с использованием generic
+ *
+ * @author Evgeny Novoselov
+ */
+public class SimpleArray {
+}
---
+++ chapter_001/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -0,0 +1,9 @@
+package ru.job4j.generic;
+
+/**
+ * Класс для тестирования обертки над массивом с использованием generic.
+ *
+ * @author Evgeny Novoselov
+ */
+public class SimpleArrayTest {
+}
