6f4b53f9d28851425c1a117cfbc0213acbd72bef
Evgeny Novoselov <zkodru@gmail.com>
2020-04-21 19:23:07 +0300

    Добавил методы

--- chapter_001/src/main/java/ru/job4j/generic/SimpleArray.java
+++ chapter_001/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -5,5 +5,53 @@ package ru.job4j.generic;
  *
  * @author Evgeny Novoselov
  */
-public class SimpleArray {
+public class SimpleArray<T> {
+    private Object[] elements;
+    private int position = 0;
+
+    public SimpleArray(int size) {
+        this.elements = new Object[size];
+    }
+
+    /**
+     * Добавляет элемент.
+     *
+     * @param model элемент.
+     */
+    public void add(T model) {
+        if (position >= elements.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        elements[position++] = model;
+    }
+
+    /**
+     * Заменяем элемент по индексу.
+     *
+     * @param index индекс.
+     * @param model элемент.
+     */
+    public void set(int index, T model) {
+        elements[index] = model;
+    }
+
+    /**
+     * Удаляет элемент и сдвигает остальные элементы, уберая промежуток.
+     *
+     * @param index индекс.
+     */
+    public void remove(int index) {
+        // todo remove element from index
+    }
+
+    /**
+     * Возвращает элемент по индексу.
+     *
+     * @param index индекс.
+     * @return элемент.
+     */
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        return (T) elements[index];
+    }
 }
--- chapter_001/src/test/java/ru/job4j/generic/SimpleArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -1,9 +1,15 @@
 package ru.job4j.generic;
 
+import org.junit.Test;
+
 /**
  * Класс для тестирования обертки над массивом с использованием generic.
  *
  * @author Evgeny Novoselov
  */
 public class SimpleArrayTest {
+    @Test
+    public void whenAddElementsThenGetsHis() {
+        SimpleArray<Integer> simpleArray = new SimpleArray<>();
+    }
 }
