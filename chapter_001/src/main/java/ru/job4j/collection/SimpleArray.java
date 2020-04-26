fa9ec3f02bbf439b949d911a5bb347622945a3f8
Evgeny Novoselov <zkodru@gmail.com>
2020-04-26 23:58:28 +0300

    5.3.1. Создать динамический список на базе массива.[#270715] (#15)
    * Добавил начальные файлы
    
    * Реализовал работу контейнера на основе массива

---
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleArray.java
@@ -0,0 +1,52 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+/**
+ * Хранилище на основе массива.
+ *
+ * @author Evgeny Novoselov
+ */
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] container = new Object[10];
+    private int position = 0;
+    private int modCount = 0;
+
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        return (T) container[Objects.checkIndex(index, position)];
+    }
+
+    public void add(T model) {
+        if (position + 1 > container.length * 0.75) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+        container[position++] = model;
+        modCount++;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int index = 0;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index < position;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) container[index++];
+            }
+        };
+    }
+}
