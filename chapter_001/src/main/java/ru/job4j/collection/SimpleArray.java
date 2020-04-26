934db62f47d618ccbbc6b3ffd340a48a5373efa8
Evgeny Novoselov <zkodru@gmail.com>
2020-04-26 23:54:34 +0300

    Реализовал работу контейнера на основе массива

--- chapter_001/src/main/java/ru/job4j/collection/SimpleArray.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleArray.java
@@ -1,6 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Хранилище на основе массива.
@@ -8,16 +8,45 @@ import java.util.Iterator;
  * @author Evgeny Novoselov
  */
 public class SimpleArray<T> implements Iterable<T> {
+    private Object[] container = new Object[10];
+    private int position = 0;
+    private int modCount = 0;
+
+    @SuppressWarnings("unchecked")
     public T get(int index) {
-        return null;
+        return (T) container[Objects.checkIndex(index, position)];
     }
 
     public void add(T model) {
-
+        if (position + 1 > container.length * 0.75) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+        container[position++] = model;
+        modCount++;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new Iterator<T>() {
+            private int index = 0;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index < position;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) container[index++];
+            }
+        };
     }
 }
