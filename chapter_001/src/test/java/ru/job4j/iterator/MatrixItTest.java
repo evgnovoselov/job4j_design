1c86fe98ad20d2069a22d41c53627e31de4480f4
Evgeny Novoselov <zkodru@gmail.com>
2020-05-05 12:37:57 +0300

    Поправил тесты

--- chapter_001/src/test/java/ru/job4j/iterator/MatrixItTest.java
+++ chapter_001/src/test/java/ru/job4j/iterator/MatrixItTest.java
@@ -1,98 +1,13 @@
 package ru.job4j.iterator;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class MatrixItTest {
-
-    private Iterator<Integer> it;
-
-    @Before
-    public void setUp() {
-        it = new MatrixIt(new int[][]{{1}, {3, 4}, {7}});
-    }
-
-    @Test
-    public void testsThatNextMethodDoesntDependsOnPriorHasNextInvocation() {
-        assertThat(it.next(), is(1));
-        assertThat(it.next(), is(3));
-        assertThat(it.next(), is(4));
-        assertThat(it.next(), is(7));
-    }
-
-    @Test
-    public void sequentialHasNextInvocationDoesntAffectRetrievalOrder() {
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(1));
-        assertThat(it.next(), is(3));
-        assertThat(it.next(), is(4));
-        assertThat(it.next(), is(7));
-    }
-
-    @Test
-    public void hasNextNextSequentialInvocation() {
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(1));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(3));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(4));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(7));
-        assertThat(it.hasNext(), is(false));
-    }
-
-    @Test(expected = NoSuchElementException.class)
-    public void whenNotHaveNextElementThenException() {
-        it = new MatrixIt(new int[][]{
-                {1},
-        });
-        it.next();
-        it.next();
-    }
-
-    @Test
-    public void whenMatrixHaveEmptyArrayThenNotProblem() {
-        it = new MatrixIt(new int[][]{
-                {},
-                {},
-                {1},
-        });
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(1));
-        assertThat(it.hasNext(), is(false));
-    }
-
-    @Test
-    public void whenMatrixHaveOneEmptyArrayThenNotProblem() {
-        it = new MatrixIt(new int[][]{
-                {},
-                {1, 2},
-                {},
-                {},
-                {3},
-                {}
-        });
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(1));
-        assertThat(it.next(), is(2));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.next(), is(3));
-        assertThat(it.hasNext(), is(false));
-    }
-
-    @Test
-    public void whenMatrixEmptyArrayThenHasNextFalse() {
-        it = new MatrixIt(new int[][]{});
-        assertThat(it.hasNext(), is(false));
-    }
 
     @Test
     public void when4El() {
