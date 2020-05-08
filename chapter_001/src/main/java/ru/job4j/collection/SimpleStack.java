b2fe9c32376b74893c2d20b7777cacb98005f2dc
Evgeny Novoselov <zkodru@gmail.com>
2020-05-09 00:16:57 +0300

    Добавил начальные файлы

---
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleStack.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+/**
+ * Простой стек.
+ *
+ * @author Evgeny Novoselov
+ */
+public class SimpleStack<T> {
+    private ForwardLinked<T> linked = new ForwardLinked<T>();
+
+    public T pop() {
+        return null;
+    }
+
+    public void push(T value) {
+
+    }
+}
