eb25f9efa3a86cddfcbc4e627f06a95638fe402f
Evgeny Novoselov <zkodru@gmail.com>
2020-05-27 23:26:49 +0300

    Добавил начальные файлы хеш-карты

---
+++ chapter_001/src/main/java/ru/job4j/collection/map/SimpleHashMap.java
@@ -0,0 +1,9 @@
+package ru.job4j.collection.map;
+
+/**
+ * Реализация простой хэш-карты.
+ *
+ * @author Evgeny Novoselov
+ */
+public class SimpleHashMap {
+}
---
+++ chapter_001/src/test/java/ru/job4j/collection/map/SimpleHashMapTest.java
@@ -0,0 +1,9 @@
+package ru.job4j.collection.map;
+
+/**
+ * Тестируем реализацию простой хэш-карты.
+ *
+ * @author Evgeny Novoselov
+ */
+public class SimpleHashMapTest {
+}
