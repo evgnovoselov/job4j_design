4bf7aacf49008262ca608d1d1801534b1fb7fa20
Evgeny Novoselov <zkodru@gmail.com>
2020-05-30 14:00:45 +0300

    Добавил тест итератора

--- chapter_001/src/test/java/ru/job4j/collection/map/SimpleHashMapTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/map/SimpleHashMapTest.java
@@ -2,6 +2,8 @@ package ru.job4j.collection.map;
 
 import org.junit.Test;
 
+import java.util.Iterator;
+
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -48,5 +50,21 @@ public class SimpleHashMapTest {
         shm.insert("one", 3);
         assertThat(shm.get("one"), is(3));
         assertThat(shm.get("two"), is(2));
+    }
+
+    /**
+     * Тест итератора.
+     */
+    @Test
+    public void whenGetIteratorThenItWorks() {
+        SimpleHashMap<String, Integer> shm = new SimpleHashMap<>();
+        shm.insert("one", 1);
+        shm.insert("two", 2);
+        Iterator<Integer> it = shm.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(1));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(2));
+        assertThat(it.hasNext(), is(false));
     }
 }
