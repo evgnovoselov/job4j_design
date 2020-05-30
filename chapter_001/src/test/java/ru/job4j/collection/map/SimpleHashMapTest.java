f43d6c61647af754f59159598e5d17db3f3bde99
Evgeny Novoselov <zkodru@gmail.com>
2020-05-30 13:52:18 +0300

    Добавил тесты

--- chapter_001/src/test/java/ru/job4j/collection/map/SimpleHashMapTest.java
+++ chapter_001/src/test/java/ru/job4j/collection/map/SimpleHashMapTest.java
@@ -1,9 +1,52 @@
 package ru.job4j.collection.map;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * Тестируем реализацию простой хэш-карты.
  *
  * @author Evgeny Novoselov
  */
 public class SimpleHashMapTest {
+    /**
+     * Тестируем добавление элементов.
+     */
+    @Test
+    public void whenTryInsertThenNotProblem() {
+        SimpleHashMap<String, Integer> shm = new SimpleHashMap<>();
+        assertThat(shm.insert("one", 1), is(true));
+        assertThat(shm.insert("two", 2), is(true));
+        assertThat(shm.insert("one", 3), is(false));
+    }
+
+    /**
+     * Тестируем получение элементов.
+     */
+    @Test
+    public void whenInsertElemThenGetHis() {
+        SimpleHashMap<String, Integer> shm = new SimpleHashMap<>();
+        shm.insert("one", 1);
+        shm.insert("two", 2);
+        shm.insert("one", 3);
+        assertThat(shm.get("one"), is(1));
+        assertThat(shm.get("two"), is(2));
+    }
+
+    /**
+     * Тестируем удаление элемента.
+     */
+    @Test
+    public void whenDeleteElemThenGetOther() {
+        SimpleHashMap<String, Integer> shm = new SimpleHashMap<>();
+        shm.insert("one", 1);
+        shm.insert("two", 2);
+        assertThat(shm.delete("one"), is(true));
+        assertThat(shm.delete("three"), is(false));
+        shm.insert("one", 3);
+        assertThat(shm.get("one"), is(3));
+        assertThat(shm.get("two"), is(2));
+    }
 }
