adaa8297ab7c0786ff1ec936adf5c3981db5d912
Evgeny Novoselov <zkodru@gmail.com>
2020-05-30 12:56:43 +0300

    Добавил пустые методы

--- chapter_001/src/main/java/ru/job4j/collection/map/SimpleHashMap.java
+++ chapter_001/src/main/java/ru/job4j/collection/map/SimpleHashMap.java
@@ -5,5 +5,35 @@ package ru.job4j.collection.map;
  *
  * @author Evgeny Novoselov
  */
-public class SimpleHashMap {
+public class SimpleHashMap<K, V> {
+    /**
+     * Добавляем элемент в карту.
+     *
+     * @param key   Ключ
+     * @param value Значение
+     * @return Результат добавления элемента.
+     */
+    public boolean insert(K key, V value) {
+        return false;
+    }
+
+    /**
+     * Возвращаем значение по ключу.
+     *
+     * @param key Ключ.
+     * @return Значение по ключу.
+     */
+    public V get(K key) {
+        return null;
+    }
+
+    /**
+     * Удаляем значение.
+     *
+     * @param key Ключ.
+     * @return Результат удаления.
+     */
+    public boolean delete(K key) {
+        return false;
+    }
 }
