5e371d49d2f73d6ee18495475e3a814e537f09e4
Evgeny Novoselov <zkodru@gmail.com>
2020-11-12 15:37:36 +0300

    Дополнил тесты

--- chapter_001/src/test/java/ru/job4j/mail/UserEmailsTest.java
+++ chapter_001/src/test/java/ru/job4j/mail/UserEmailsTest.java
@@ -30,4 +30,36 @@ public class UserEmailsTest {
                 "user3", Set.of("xyz@pisem.net", "vasya@pupkin.com")
         )));
     }
+
+    /**
+     * При добавлении всех пользователей с одинаковой почтой должно произойти слияние пользователей.
+     */
+    @Test
+    public void whenAddUsersSameEmailsThenMergeAll() {
+        Map<String, Set<String>> userEmails = new LinkedHashMap<>();
+        userEmails.put("user1", Set.of("xxx@ya.ru", "foo@gmail.com"));
+        userEmails.put("user2", Set.of("xxx@ya.ru", "foo@gmail.com"));
+        userEmails.put("user3", Set.of("xxx@ya.ru", "foo@gmail.com"));
+        Map<String, Set<String>> expected = UserEmails.merge(userEmails);
+        assertThat(expected, is(Map.of(
+                "user1", Set.of("xxx@ya.ru", "foo@gmail.com")
+        )));
+    }
+
+    /**
+     * При добавлении пользователей с разными почтами слияние пользователей не происходит.
+     */
+    @Test
+    public void whenAddUsersDiffEmailsThenNotMerge() {
+        Map<String, Set<String>> userEmails = new LinkedHashMap<>();
+        userEmails.put("user1", Set.of("eee@ya.ru", "fff@gmail.com", "ooo@mail.ru"));
+        userEmails.put("user2", Set.of("xxx@ya.ru", "xxx@gmail.com", "ddd@mail.ru"));
+        userEmails.put("user3", Set.of("zzz@ya.ru", "ttt@gmail.com", "ccc@mail.ru"));
+        Map<String, Set<String>> expected = UserEmails.merge(userEmails);
+        assertThat(expected, is(Map.of(
+                "user1", Set.of("eee@ya.ru", "fff@gmail.com", "ooo@mail.ru"),
+                "user2", Set.of("xxx@ya.ru", "xxx@gmail.com", "ddd@mail.ru"),
+                "user3", Set.of("zzz@ya.ru", "ttt@gmail.com", "ccc@mail.ru")
+        )));
+    }
 }
