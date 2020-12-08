e6e6e00606481ee47fe46dab9f9b678f3d505890
Evgeny Novoselov <zkodru@gmail.com>
2020-12-08 14:33:54 +0300

    Реализовал задачу

--- chapter_002/src/main/java/ru/job4j/io/LogFilter.java
+++ chapter_002/src/main/java/ru/job4j/io/LogFilter.java
@@ -1,17 +1,28 @@
 package ru.job4j.io;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Класс для фильтрации лога на наличие ошибки 404
  */
 public class LogFilter {
     public static List<String> filter(String file) {
-        return null;
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            lines = in.lines().filter(l -> l.contains(" 404 ")).collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
     }
 
     public static void main(String[] args) {
         List<String> log = filter("files/log.txt");
-        System.out.println(log);
+        log.forEach(System.out::println);
     }
 }
