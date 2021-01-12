b432b325948e93ed6964b26a2cf80633bb2f1b2d
Evgeny Novoselov <zkodru@gmail.com>
2021-01-12 16:20:21 +0300

    Дописал тесты и метод анализа лога

--- chapter_002/src/main/java/ru/job4j/io/Analyze.java
+++ chapter_002/src/main/java/ru/job4j/io/Analyze.java
@@ -1,17 +1,38 @@
 package ru.job4j.io;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
+/**
+ * Класс анализа лога сервера.
+ */
 public class Analyze {
+    /**
+     * Метод проверяет файл лога доступности сервера в формате <code>type date</code>
+     * и создает файл с отрезками времени недоступностью сервера в формате <code>time_down;time_up;</code>.
+     *
+     * @param source Путь к файлу лога сервера.
+     * @param target Путь к файлу анализа лога.
+     */
     public void unavailable(String source, String target) {
-    }
-
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("files/unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
+        try (BufferedReader in = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            String line;
+            boolean unavailable = false;
+            while ((line = in.readLine()) != null) {
+                String time = line.substring(line.indexOf(" ") + 1) + ";";
+                if (!unavailable) {
+                    if (line.startsWith("400") || line.startsWith("500")) {
+                        unavailable = true;
+                        out.write(time);
+                    }
+                }
+                if (unavailable) {
+                    if (line.startsWith("200") || line.startsWith("300")) {
+                        unavailable = false;
+                        out.write(time + System.lineSeparator());
+                    }
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,4 +1,87 @@
 package ru.job4j.io;
 
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Класс для проверки метода анализа лога доступности сервера.
+ */
 public class AnalyzeTest {
+    private final String target = "../files/test/unavailable.csv";
+
+    /**
+     * Метод проверяет отрезки недоступности сервера.
+     */
+    @Test
+    public void whenHaveDownTimeThenTime() {
+        Analyze analyze = new Analyze();
+        String source = "../files/test/server_down_time.log";
+        analyze.unavailable(source, target);
+        List<String> expected = null;
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            expected = in.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(expected, is(List.of("10:57:01;10:59:01;", "11:01:02;11:02:02;")));
+    }
+
+    /**
+     * Метод проверяет лог, когда сервер перестал быть доступен совсем.
+     */
+    @Test
+    public void whenServerUpDownAndNotUpThenNotHaveUpTime() {
+        Analyze analyze = new Analyze();
+        String source = "../files/test/server_up_down_and_not_up.log";
+        analyze.unavailable(source, target);
+        List<String> expected = null;
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            expected = in.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(expected, is(List.of("10:57:01;")));
+    }
+
+    /**
+     * Метод проверяет лог, когда сервер не был изначально доступен и до конца лога.
+     */
+    @Test
+    public void whenDownThenHaveDownTime() {
+        Analyze analyze = new Analyze();
+        String source = "../files/test/server_down.log";
+        analyze.unavailable(source, target);
+        List<String> expected = null;
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            expected = in.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(expected, is(List.of("10:57:01;")));
+    }
+
+    /**
+     * Метод проверяет лог, когда сервер был не доступен и заработал.
+     */
+    @Test
+    public void whenDownUpThenHaveTime() {
+        Analyze analyze = new Analyze();
+        String source = "../files/test/server_down_up.log";
+        analyze.unavailable(source, target);
+        List<String> expected = null;
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            expected = in.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertThat(expected, is(List.of("10:57:01;11:02:02;")));
+    }
 }
