e723d16f3934c84fbf541b5c833db0871d0f10fd
Evgeny Novoselov <zkodru@gmail.com>
2021-03-30 22:43:38 +0300

    Сделал выход с сервера

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -13,15 +13,20 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            while (!server.isClosed()) {
+            boolean isActive = true;
+            while (isActive && !server.isClosed()) {
                 Socket socket = server.accept();
                 try (
                         OutputStream out = socket.getOutputStream();
                         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
                 ) {
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
+                    String str = in.readLine();
+                    while (!str.isEmpty()) {
+                        if (str.startsWith("GET") && str.contains("msg=Bye")) {
+                            isActive = false;
+                        }
                         System.out.println(str);
+                        str = in.readLine();
                     }
                     out.write("HTTP/1.1 200 OK \r\n".getBytes());
                 }
