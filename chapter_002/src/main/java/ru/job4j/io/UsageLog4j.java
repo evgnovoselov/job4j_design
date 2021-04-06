afe2a14ce2d8a49b75597682e10c9a619a0026a2
Evgeny Novoselov <zkodru@gmail.com>
2021-04-06 12:11:39 +0300

    Добавил вывод переменных в логгер

--- chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
+++ chapter_002/src/main/java/ru/job4j/io/UsageLog4j.java
@@ -15,5 +15,17 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+        boolean bool = false;
+        byte b = 32;
+        char ch = 'W';
+        short sh = 3200;
+        int i = 1234567;
+        long l = 4223134;
+        double d = 7654321;
+        float fl = 8573521;
+        LOG.debug(
+                "Переменные:\nboolean = {},\nbyte = {},\nchar = {},\nshort = {},\nint = {},\nlong = {},\ndouble = {},\nfloat = {}",
+                bool, b, ch, sh, i, l, d, fl
+        );
     }
 }
