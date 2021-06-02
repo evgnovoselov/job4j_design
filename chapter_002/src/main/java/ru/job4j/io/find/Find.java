928d268d5ba50d47de48c2ae889b3d57d89b5c3d
Evgeny Novoselov <zkodru@gmail.com>
2021-06-02 16:23:15 +0300

    Дополнил класс поиска проверками аргументов

--- chapter_002/src/main/java/ru/job4j/io/find/Find.java
+++ chapter_002/src/main/java/ru/job4j/io/find/Find.java
@@ -1,5 +1,9 @@
 package ru.job4j.io.find;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Set;
+
 /**
  * Класс ищет файл в заданном каталоге и подкаталогах.
  * Ключи
@@ -10,9 +14,16 @@ package ru.job4j.io.find;
  */
 public class Find {
     public static void main(String[] args) {
+        Set<String> types = Set.of("mask", "name", "regex");
         if (args.length < 4) {
             throw new IllegalArgumentException("Error argument count.");
         }
         Arguments arguments = new Arguments(args);
+        if (!Files.isDirectory(Path.of(arguments.directory()))) {
+            throw new IllegalArgumentException(String.format("Error! Not directory: %s", arguments.directory()));
+        }
+        if (!types.contains(arguments.type())) {
+            throw new IllegalArgumentException(String.format("Error! Not valid type argument: %s, need: %s", arguments.type(), types));
+        }
     }
 }
