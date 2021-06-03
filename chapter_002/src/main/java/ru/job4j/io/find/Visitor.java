6fd6dab04fe4120a1ab3b7ff69229973c7e468f6
Evgeny Novoselov <zkodru@gmail.com>
2021-06-03 10:30:50 +0300

    Добавил класс визитора

---
+++ chapter_002/src/main/java/ru/job4j/io/find/Visitor.java
@@ -0,0 +1,30 @@
+package ru.job4j.io.find;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Класс просматривает папки и подпапки в поисках файла по предикату.
+ */
+public class Visitor extends SimpleFileVisitor<Path> {
+    private Predicate<Path> predicate;
+    private List<Path> paths = new ArrayList<>();
+
+    public Visitor(Predicate<Path> predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (predicate.test(file)) {
+            paths.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
