bfa50f00583b080d3861c9ae0e7763ffccd0121d
Evgeny Novoselov <zkodru@gmail.com>
2021-06-15 14:33:42 +0300

    Убрал лишний класс

--- chapter_002/src/main/java/ru/job4j/io/find/Find.java
+++ chapter_002/src/main/java/ru/job4j/io/find/Find.java
@@ -21,6 +21,57 @@ public class Find {
         Set<String> types = Set.of("mask", "name", "regex");
         Arguments arguments = new Arguments(args);
         arguments.validate(types);
-        FindFile.write(arguments.output(), FindFile.search(arguments.directory(), arguments.type(), arguments.name()));
+        write(arguments.output(), search(arguments.directory(), arguments.type(), arguments.name()));
+    }
+
+    public static List<Path> search(String directory, String type, String name) {
+        Visitor visitor = new Visitor(path -> false);
+        if (type.equals("name")) {
+            visitor = new Visitor(path -> path.getFileName().toString().equals(name));
+        }
+        if (type.equals("regex")) {
+            visitor = new Visitor(path -> path.getFileName().toString().matches(name));
+        }
+        if (type.equals("mask")) {
+            visitor = new Visitor(path -> path.getFileName().toString().matches(maskToRegex(name)));
+        }
+        try {
+            Files.walkFileTree(Path.of(directory), visitor);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return visitor.getPaths();
+    }
+
+    public static void write(String output, List<Path> paths) {
+        try (BufferedWriter writer = Files.newBufferedWriter(Path.of(output))) {
+            for (Path file : paths) {
+                writer.write(file.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String maskToRegex(String text) {
+        StringBuilder reg = new StringBuilder();
+        for (byte b : text.getBytes(StandardCharsets.UTF_8)) {
+            char ch = (char) b;
+            if (ch == '*') {
+                reg.append(".*");
+                continue;
+            }
+            if (ch == '.') {
+                reg.append("\\.");
+                continue;
+            }
+            if (ch == '?') {
+                reg.append(".");
+                continue;
+            }
+            reg.append(ch);
+        }
+        return reg.toString();
     }
 }
