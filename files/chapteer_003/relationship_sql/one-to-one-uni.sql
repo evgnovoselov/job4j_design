c633e60df432842aed4416e0256ea24a43a52d37
Evgeny Novoselov <zkodru@gmail.com>
2021-06-30 16:30:07 +0300

    Добавил изображение связией и sql файлы для их создания

---
+++ files/chapteer_003/relationship_sql/one-to-one-uni.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE contact (
+  id SERIAL PRIMARY KEY,
+  phone VARCHAR(255),
+  comment TEXT
+);
+
+CREATE TABLE people (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  contact_id INT REFERENCES contact(id) UNIQUE
+);
+
+INSERT INTO contact (phone, comment) VALUES ('+7-912-345-6789', 'ООО Ромашка');
+INSERT INTO people (name, contact_id) VALUES ('Yulia Yulyashina', 1);
