cd560037332ca49a15e170b291c33c9a2b2a4efb
Evgeny Novoselov <zkodru@gmail.com>
2021-07-28 01:48:51 +0300

    Опробовал PrepareStatemant

---
+++ chapter_003/src/main/java/ru/job4j/jdbc/city/City.java
@@ -0,0 +1,61 @@
+package ru.job4j.jdbc.city;
+
+import java.util.Objects;
+
+public class City {
+    private int id;
+    private String name;
+    private int population;
+
+    public City(int id, String name, int population) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        City city = (City) o;
+        return id == city.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", population=" + population +
+                '}';
+    }
+}
