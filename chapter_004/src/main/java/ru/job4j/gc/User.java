0154b3a426a87c24334858c15248a1fec3c2f8d8
Evgeny Novoselov <zkodru@gmail.com>
2021-10-06 16:41:24 +0300

    Добавил расчет занимаемого места в памяти объекта

--- chapter_004/src/main/java/ru/job4j/gc/User.java
+++ chapter_004/src/main/java/ru/job4j/gc/User.java
@@ -3,7 +3,17 @@ package ru.job4j.gc;
 import java.util.Objects;
 
 public class User {
+    /*
+    Заголовок: 16 byte (32bit = 8 byte, 64bit = 16 byte)
+    Поля int: 4 byte * 3 = 12 byte
+    Ссылочная переменная на объект массива: 8 byte (32bit = 4 byte, 64bit = 8 byte)
+    Итого, без new char[]: 36 byte (в размер не входит информация о массиве new char[] где хранятся данные)
+    */
     private String name;
+
+    /*
+    Поле int: 4 byte
+    */
     private int age;
 
     public User(String name, int age) {
@@ -50,5 +60,42 @@ public class User {
     }
 
     public static void main(String[] args) {
+        /*
+        Ниже экземпляр класса User занимает памяти:
+        Заголовок: 16 byte
+        Поле String {
+            Поля String без new char[] расчитал в объявлении класса: 36 byte
+            Поле new char[6] {
+                Заголовок: 16 byte + 4 byte на длину массива == 20 byte
+                Примитивы char: 2 byte * 6 == 12 byte
+                Выравнивание для кратности 16: 0 byte
+                Итого: 32 byte
+            }
+        }: 36 byte + 32 byte = 68 byte
+        Поле int: 4 byte
+        Выравнивание для кратности 16: 8 byte
+        Итого: 96 byte
+         */
+        User user = new User("Evgeny", 32);
+
+        /*
+        Ниже экземпляр класса User занимает памяти:
+        Заголовок: 16 byte
+        Поле String {
+            Поля String без new char[] расчитал в объявлении класса: 36 byte
+            Поле new char[5] {
+                Заголовок: 16 byte + 4 byte на длину массива == 20 byte
+                Примитивы char: 2 byte * 5 == 10 byte
+                Выравнивание для кратности 16: 2 byte
+                Итого: 32 byte
+            }
+        }: 36 byte + 32 byte = 68 byte
+        Поле int: 4 byte
+        Выравнивание для кратности 16: 8 byte
+        Итого: 96 byte
+         */
+        User user1 = new User("Julia", 30);
+        System.out.println(user);
+        System.out.println(user1);
     }
 }
