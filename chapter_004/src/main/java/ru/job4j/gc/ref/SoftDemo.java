482861d289f3248e1facdd77095121fc40767d11
Evgeny Novoselov <zkodru@gmail.com>
2021-10-15 19:13:51 +0300

    Дополнил метод пример про мягкие ссылки

--- chapter_004/src/main/java/ru/job4j/gc/ref/SoftDemo.java
+++ chapter_004/src/main/java/ru/job4j/gc/ref/SoftDemo.java
@@ -13,6 +13,10 @@ public class SoftDemo {
         example1();
         System.out.println("example2");
         example2();
+        System.out.println("unsafe");
+        unsafe();
+        System.out.println("safe");
+        safe();
     }
 
     private static void example1() {
@@ -24,7 +28,7 @@ public class SoftDemo {
 
     private static void example2() {
         List<SoftReference<Object>> objects = new ArrayList<>();
-        for (int i = 0; i < 10_000_000; i++) {
+        for (int i = 0; i < 1_000_000; i++) {
             objects.add(new SoftReference<>(new Object() {
                 String value = String.valueOf(System.currentTimeMillis());
 
@@ -43,5 +47,35 @@ public class SoftDemo {
             }
         }
         System.out.println(liveObject);
+    }
+
+    /**
+     * Данный метод демонстрирует неверное получение объекта по мягкой ссылке.
+     */
+    private static void unsafe() {
+        List<SoftReference<Object>> someData = new ArrayList<>();
+        someData.add(new SoftReference<>(new Object()));
+        if (someData.get(0).get() != null) {
+            // do something
+        } else {
+            //do something
+        }
+        // do something
+        Object object = someData.get(0).get();
+    }
+
+    /**
+     * Данный метод демонстрирует правильное получение объекта по мягкой сылкой.
+     */
+    private static void safe() {
+        List<SoftReference<Object>> someData = new ArrayList<>();
+        someData.add(new SoftReference<>(new Object()));
+        Object strong = someData.get(0).get();
+        if (strong != null) {
+            // do something
+        } else {
+            // do something
+        }
+        // work with strong
     }
 }
