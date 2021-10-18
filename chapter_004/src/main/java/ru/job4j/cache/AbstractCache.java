880bf7e261f5426fc3afe02908b237d1c42ce035
Evgeny Novoselov <zkodru@gmail.com>
2021-10-18 19:16:15 +0300

    1. Реализация кеша на SoftReference [#1592] (#94)

---
+++ chapter_004/src/main/java/ru/job4j/cache/AbstractCache.java
@@ -0,0 +1,27 @@
+package ru.job4j.cache;
+
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Базовый класс для работы с кешем.
+ */
+public abstract class AbstractCache<K, V> {
+    protected final Map<K, SoftReference<V>> cache = new HashMap<>();
+
+    public void put(K key, V value) {
+        cache.put(key, new SoftReference<>(value));
+    }
+
+    public V get(K key) {
+        V result = null;
+        SoftReference<V> reference = cache.get(key);
+        if (reference != null) {
+            result = reference.get();
+        }
+        return result;
+    }
+
+    protected abstract V load(K key);
+}
