c7b530626d4b95756c1a22bc4572c31dd264fb4d
Evgeny Novoselov <zkodru@gmail.com>
2021-10-18 12:32:15 +0300

    Дополнил про слабые ссылки

--- chapter_004/src/main/java/ru/job4j/gc/ref/WeakDemo.java
+++ chapter_004/src/main/java/ru/job4j/gc/ref/WeakDemo.java
@@ -1,5 +1,6 @@
 package ru.job4j.gc.ref;
 
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,8 @@ public class WeakDemo {
         example2();
         System.out.println("safe");
         safe();
+        System.out.println("example3");
+        example3();
     }
 
     public static void example1() throws InterruptedException {
@@ -61,5 +64,24 @@ public class WeakDemo {
             // do something
         }
         // work with strong
+    }
+
+    /**
+     * Демонстрация ReferenceQueue.
+     */
+    private static void example3() throws InterruptedException {
+        Object object = new Object() {
+            @Override
+            protected void finalize() throws Throwable {
+                System.out.println("Removed! example3");
+            }
+        };
+        ReferenceQueue<Object> queue = new ReferenceQueue<>();
+        WeakReference<Object> weak = new WeakReference<>(object, queue);
+        object = null;
+        System.gc();
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("from link " + weak.get());
+        System.out.println("from queue " + queue.poll());
     }
 }
