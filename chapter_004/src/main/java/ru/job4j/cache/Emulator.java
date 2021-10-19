458e21f4cf11c4f9f5c953b5422095f7d1db39d1
Evgeny Novoselov <zkodru@gmail.com>
2021-10-19 13:33:03 +0300

    Добавил меню в Emulator

--- chapter_004/src/main/java/ru/job4j/cache/Emulator.java
+++ chapter_004/src/main/java/ru/job4j/cache/Emulator.java
@@ -1,5 +1,7 @@
 package ru.job4j.cache;
 
+import java.util.Scanner;
+
 /**
  * Класс демонстрирующий работу кеша.
  */
@@ -10,5 +12,24 @@ public class Emulator {
         cache.load("Address.txt");
         System.out.println(cache.load("Names.txt"));
         System.out.println(cache.load("Address.txt"));
+        boolean run = true;
+        int action = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (run) {
+            printMenu();
+            System.out.print("Выберите пункт меню: ");
+            action = scanner.nextInt();
+            if (action == 0) {
+                run = false;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("=== Меню");
+        System.out.println("=== 1. Выбрать директорию");
+        System.out.println("=== 2. Добавить файл в кеш");
+        System.out.println("=== 3. Получить содержимое файла");
+        System.out.println("=== 0. Выйти из программы");
     }
 }
