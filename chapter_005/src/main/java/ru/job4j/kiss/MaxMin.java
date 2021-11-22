0f521d4515a232dc7269b4fa9e25233a6029e56b
Evgeny Novoselov <zkodru@gmail.com>
2021-11-22 11:26:13 +0300

    Добавил реализацию классов поска минимального и максимального значения в списке

--- chapter_005/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_005/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -3,12 +3,42 @@ package ru.job4j.kiss;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Класс поиска в списке максимального значения и минимального.
+ */
 public class MaxMin {
+    /**
+     * Возвращает максимальное значение в списке.
+     *
+     * @param value      Список.
+     * @param comparator Компаратор.
+     * @param <T>        Тип значений.
+     * @return Максимальное значение в списке.
+     */
     public <T> T max(List<T> value, Comparator<T> comparator) {
-        return null;
+        T result = null;
+        var iterator = value.iterator();
+        if (iterator.hasNext()) {
+            result = iterator.next();
+        }
+        while (iterator.hasNext()) {
+            T current = iterator.next();
+            if (comparator.compare(result, current) > 0) {
+                result = current;
+            }
+        }
+        return result;
     }
 
+    /**
+     * Возвращает минимальное значение в списке.
+     *
+     * @param value      Список.
+     * @param comparator Компаратор.
+     * @param <T>        Тип значений.
+     * @return Минимальное значение в списке.
+     */
     public <T> T min(List<T> value, Comparator<T> comparator) {
-        return null;
+        return max(value, comparator.reversed());
     }
 }
---
+++ chapter_005/src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.kiss;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxMinTest {
+
+    @Test
+    public void whenThen() {
+    }
+
+    @Test
+    public void whenThen2() {
+    }
+}
