fee231bcbc283c2686cd037fe86c7625a6784d28
Evgeny Novoselov <zkodru@gmail.com>
2021-11-22 20:05:48 +0300

    Поправил чекстайл

--- chapter_005/src/main/java/ru/job4j/tdd/Session3D.java
+++ chapter_005/src/main/java/ru/job4j/tdd/Session3D.java
@@ -3,12 +3,16 @@ package ru.job4j.tdd;
 import java.util.Objects;
 
 public class Session3D implements Session {
-    private int id =1;
+    private int id = 1;
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Session3D session3D = (Session3D) o;
         return id == session3D.id;
     }
