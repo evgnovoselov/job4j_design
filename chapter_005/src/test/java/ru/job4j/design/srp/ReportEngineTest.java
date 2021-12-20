c04fdc8fa4d792b4beb34d119808bc03497242dc
Evgeny Novoselov <zkodru@gmail.com>
2021-12-20 15:06:07 +0300

    Добавил тест отчета

---
+++ chapter_005/src/test/java/ru/job4j/design/srp/ReportEngineTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.design.srp;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.junit.Assert.*;
+
+/**
+ * Тест отчетов.
+ */
+public class ReportEngineTest {
+
+    @Test
+    public void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new ReportEngine(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(worker.getHired()).append(";")
+                .append(worker.getFired()).append(";")
+                .append(worker.getSalary()).append(";")
+                .append(System.lineSeparator());
+        assertEquals(engine.generate(employee -> true), expect.toString());
+    }
+}
