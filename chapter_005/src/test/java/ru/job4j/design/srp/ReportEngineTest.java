37984eaff1b89910202097b2564f2ada7f64ac34
Evgeny Novoselov <zkodru@gmail.com>
2021-12-21 00:38:02 +0300

    Добавил генерацию отчета для HR

--- chapter_005/src/test/java/ru/job4j/design/srp/ReportEngineTest.java
+++ chapter_005/src/test/java/ru/job4j/design/srp/ReportEngineTest.java
@@ -2,7 +2,9 @@ package ru.job4j.design.srp;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Comparator;
 
 import static org.junit.Assert.*;
 
@@ -84,6 +86,41 @@ public class ReportEngineTest {
                 .append(worker.getHired()).append(";")
                 .append(worker.getFired()).append(";")
                 .append(String.format("%.2f руб.", worker.getSalary())).append(";")
+                .append(System.lineSeparator());
+        assertEquals(engine.generate(employee -> true), expect.toString());
+    }
+
+    @Test
+    public void whenGeneratedReportOrderBySalaryDescAndWithoutHiredFired() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee[] workers = new Employee[]{
+                new Employee("Ivan", now, now, 100),
+                new Employee("Petr", now, now, 300),
+                new Employee("Alex", now, now, 400),
+                new Employee("Oleg", now, now, 200),
+        };
+        Arrays.stream(workers).forEach(store::add);
+        Report engine = new ReportEngineWithComparatorWithoutHiredFired(store, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return Double.compare(o1.getSalary(), o2.getSalary());
+            }
+        }.reversed());
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(workers[2].getName()).append(";")
+                .append(workers[2].getSalary()).append(";")
+                .append(System.lineSeparator())
+                .append(workers[1].getName()).append(";")
+                .append(workers[1].getSalary()).append(";")
+                .append(System.lineSeparator())
+                .append(workers[3].getName()).append(";")
+                .append(workers[3].getSalary()).append(";")
+                .append(System.lineSeparator())
+                .append(workers[0].getName()).append(";")
+                .append(workers[0].getSalary()).append(";")
                 .append(System.lineSeparator());
         assertEquals(engine.generate(employee -> true), expect.toString());
     }
