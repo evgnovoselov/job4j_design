da09e0f1a150911957d6a8c06380211d9d2c5645
Evgeny Novoselov <zkodru@gmail.com>
2021-12-28 15:37:51 +0300

    Добавил частичную реализацию теста

--- chapter_005/src/test/java/ru/job4j/design/srp/ReportEngineXmlTest.java
+++ chapter_005/src/test/java/ru/job4j/design/srp/ReportEngineXmlTest.java
@@ -2,7 +2,9 @@ package ru.job4j.design.srp;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Calendar;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Тестовый класс генерации отчета в Xml.
@@ -13,5 +15,12 @@ public class ReportEngineXmlTest {
      */
     @Test
     public void whenGenerateXmlReportThenValidReport() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        store.add(new Employee("Evgeny", now, now, 700));
+        store.add(new Employee("Ivan", now, now, 300));
+        store.add(new Employee("Julia", now, now, 400));
+        Report engine = new ReportEngineXml(store);
+        assertEquals("TODO", engine.generate(employee -> true));
     }
 }
