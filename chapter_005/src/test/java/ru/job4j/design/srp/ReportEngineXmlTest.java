c5691fdea72128227a82885c63282e3f7c7fc08a
Evgeny Novoselov <zkodru@gmail.com>
2021-12-28 15:24:08 +0300

    Добавил класс для теста

---
+++ chapter_005/src/test/java/ru/job4j/design/srp/ReportEngineXmlTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.design.srp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ReportEngineXmlTest {
+
+    @Test
+    public void generate() {
+    }
+}
