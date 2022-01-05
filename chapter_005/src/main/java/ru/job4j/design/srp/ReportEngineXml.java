9c043f99499d00ebb1db5d14aac461a354afbf65
Evgeny Novoselov <zkodru@gmail.com>
2022-01-05 19:12:08 +0300

    Сделал представление отчета в формате xml

--- chapter_005/src/main/java/ru/job4j/design/srp/ReportEngineXml.java
+++ chapter_005/src/main/java/ru/job4j/design/srp/ReportEngineXml.java
@@ -1,5 +1,11 @@
 package ru.job4j.design.srp;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -16,6 +22,33 @@ public class ReportEngineXml implements Report {
     @Override
     public String generate(Predicate<Employee> filter) {
         List<Employee> employees = store.findBy(filter);
-        return null;
+        String xml = "";
+        try {
+            JAXBContext context = JAXBContext.newInstance(Employees.class);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            try (StringWriter writer = new StringWriter()) {
+                marshaller.marshal(new Employees(employees), writer);
+                xml = writer.getBuffer().toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return xml;
+    }
+
+    @XmlRootElement(name = "employees")
+    private static class Employees {
+        @XmlElement(name = "employee")
+        private List<Employee> employees;
+
+        public Employees() {
+        }
+
+        public Employees(List<Employee> employees) {
+            this.employees = employees;
+        }
     }
 }
--- chapter_005/src/test/java/ru/job4j/design/srp/ReportEngineXmlTest.java
+++ chapter_005/src/test/java/ru/job4j/design/srp/ReportEngineXmlTest.java
@@ -2,6 +2,7 @@ package ru.job4j.design.srp;
 
 import org.junit.Test;
 
+import javax.xml.bind.DatatypeConverter;
 import java.util.Calendar;
 
 import static org.junit.Assert.assertEquals;
@@ -21,6 +22,28 @@ public class ReportEngineXmlTest {
         store.add(new Employee("Ivan", now, now, 300));
         store.add(new Employee("Julia", now, now, 400));
         Report engine = new ReportEngineXml(store);
-        assertEquals("TODO", engine.generate(employee -> true));
+        StringBuilder expectedFormat = new StringBuilder("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n");
+        expectedFormat.append("<employees>\n");
+        expectedFormat.append("    <employee>\n");
+        expectedFormat.append("        <fired>%1$s</fired>\n");
+        expectedFormat.append("        <hired>%1$s</hired>\n");
+        expectedFormat.append("        <name>Evgeny</name>\n");
+        expectedFormat.append("        <salary>700.0</salary>\n");
+        expectedFormat.append("    </employee>\n");
+        expectedFormat.append("    <employee>\n");
+        expectedFormat.append("        <fired>%1$s</fired>\n");
+        expectedFormat.append("        <hired>%1$s</hired>\n");
+        expectedFormat.append("        <name>Ivan</name>\n");
+        expectedFormat.append("        <salary>300.0</salary>\n");
+        expectedFormat.append("    </employee>\n");
+        expectedFormat.append("    <employee>\n");
+        expectedFormat.append("        <fired>%1$s</fired>\n");
+        expectedFormat.append("        <hired>%1$s</hired>\n");
+        expectedFormat.append("        <name>Julia</name>\n");
+        expectedFormat.append("        <salary>400.0</salary>\n");
+        expectedFormat.append("    </employee>\n");
+        expectedFormat.append("</employees>\n");
+        String expected = String.format(expectedFormat.toString(), DatatypeConverter.printDateTime(now));
+        assertEquals(expected, engine.generate(employee -> true));
     }
 }
