d746b8a1b6abc92a2cba88499ceac8c6d96aa854
Evgeny Novoselov <zkodru@gmail.com>
2022-01-11 13:13:39 +0300

    Поправил первую неправильную реализацию LSP

--- chapter_005/src/main/java/ru/job4j/ood/lsp/bad1/Manager.java
+++ chapter_005/src/main/java/ru/job4j/ood/lsp/bad1/Manager.java
@@ -3,6 +3,7 @@ package ru.job4j.ood.lsp.bad1;
 public class Manager extends Employee {
     public Manager(int salary) {
         super(salary);
+        checkSalary(salary);
     }
 
     @Override
@@ -10,9 +11,13 @@ public class Manager extends Employee {
         if (money < 0 && salary + money < 0) {
             throw new IllegalArgumentException("Зарплату нельзя сделать меньше 0");
         }
-        if (salary + money > 150) {
+        checkSalary(salary + money);
+        salary += money;
+    }
+
+    private void checkSalary(int salary) {
+        if (salary > 150) {
             throw new IllegalArgumentException("Зарплата менеджера станет слишком большой");
         }
-        salary += money;
     }
 }
