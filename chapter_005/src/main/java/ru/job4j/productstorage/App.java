ae8ab85f0efc80c7354b5c1b54ef4807b809e5f7
Evgeny Novoselov <zkodru@gmail.com>
2022-01-13 12:01:58 +0300

    Разнообразил продукты

--- chapter_005/src/main/java/ru/job4j/productstorage/App.java
+++ chapter_005/src/main/java/ru/job4j/productstorage/App.java
@@ -7,24 +7,56 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        List<Food> impFoods = new ArrayList<>(List.of(
+        List<Food> impFoods = generateProducts();
+        LocalDate now = LocalDate.now();
+        now = now.plusDays(2);
+        System.out.println(impFoods.get(0).getExpiryPercentOfDate(now));
+    }
+
+    public static List<Food> generateProducts() {
+        return new ArrayList<>(List.of(
                 new Food(
                         "Креветки",
                         LocalDate.of(2022, Month.JANUARY, 13),
                         LocalDate.of(2022, Month.JANUARY, 23),
                         100,
-                        5
+                        0
                 ),
                 new Food(
-                        "Креветки Имперские",
+                        "Форель",
                         LocalDate.of(2022, Month.JANUARY, 13),
                         LocalDate.of(2022, Month.JANUARY, 20),
                         100,
-                        5
+                        0
+                ),
+                new Food(
+                        "Чипсы Принглс",
+                        LocalDate.of(2022, Month.JANUARY, 13),
+                        LocalDate.of(2022, Month.JANUARY, 20),
+                        100,
+                        0
+                ),
+                new Food(
+                        "Конфеты",
+                        LocalDate.of(2022, Month.JANUARY, 13),
+                        LocalDate.of(2022, Month.JANUARY, 20),
+                        100,
+                        0
+                ),
+                new Food(
+                        "Сникерс",
+                        LocalDate.of(2022, Month.JANUARY, 13),
+                        LocalDate.of(2022, Month.JANUARY, 20),
+                        100,
+                        0
+                ),
+                new Food(
+                        "Яблоки",
+                        LocalDate.of(2022, Month.JANUARY, 13),
+                        LocalDate.of(2022, Month.JANUARY, 20),
+                        100,
+                        0
                 )
         ));
-        LocalDate now = LocalDate.now();
-        now = now.plusDays(2);
-        System.out.println(impFoods.get(0).getExpiryPercentOfDate(now));
     }
 }
