0d3de1af8eec5727d72e1d9c61b41801182b6c91
Evgeny Novoselov <zkodru@gmail.com>
2022-01-13 12:26:38 +0300

    Настроил сроки годности продуктов

--- chapter_005/src/main/java/ru/job4j/productstorage/App.java
+++ chapter_005/src/main/java/ru/job4j/productstorage/App.java
@@ -1,59 +1,60 @@
 package ru.job4j.productstorage;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
         List<Food> impFoods = generateProducts();
-        LocalDate now = LocalDate.now();
-        now = now.plusDays(2);
-        System.out.println(impFoods.get(0).getExpiryPercentOfDate(now));
+        LocalDate date = LocalDate.now();
+        date = date.plusDays(10);
+        for (Food food : impFoods) {
+            System.out.println(food.getExpiryPercentOfDate(date));
+        }
     }
 
     public static List<Food> generateProducts() {
         return new ArrayList<>(List.of(
                 new Food(
                         "Креветки",
-                        LocalDate.of(2022, Month.JANUARY, 13),
-                        LocalDate.of(2022, Month.JANUARY, 23),
+                        LocalDate.now(),
+                        LocalDate.now().plusDays(30),
                         100,
                         0
                 ),
                 new Food(
                         "Форель",
-                        LocalDate.of(2022, Month.JANUARY, 13),
-                        LocalDate.of(2022, Month.JANUARY, 20),
+                        LocalDate.now(),
+                        LocalDate.now().plusDays(12),
                         100,
                         0
                 ),
                 new Food(
                         "Чипсы Принглс",
-                        LocalDate.of(2022, Month.JANUARY, 13),
-                        LocalDate.of(2022, Month.JANUARY, 20),
+                        LocalDate.now(),
+                        LocalDate.now().plusDays(70),
                         100,
                         0
                 ),
                 new Food(
                         "Конфеты",
-                        LocalDate.of(2022, Month.JANUARY, 13),
-                        LocalDate.of(2022, Month.JANUARY, 20),
+                        LocalDate.now(),
+                        LocalDate.now().plusDays(10),
                         100,
                         0
                 ),
                 new Food(
                         "Сникерс",
-                        LocalDate.of(2022, Month.JANUARY, 13),
-                        LocalDate.of(2022, Month.JANUARY, 20),
+                        LocalDate.now(),
+                        LocalDate.now().plusDays(10),
                         100,
                         0
                 ),
                 new Food(
                         "Яблоки",
-                        LocalDate.of(2022, Month.JANUARY, 13),
-                        LocalDate.of(2022, Month.JANUARY, 20),
+                        LocalDate.now(),
+                        LocalDate.now().plusDays(13),
                         100,
                         0
                 )
