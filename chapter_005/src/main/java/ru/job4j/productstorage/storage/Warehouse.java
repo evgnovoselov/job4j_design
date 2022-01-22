efa62df1d359c133ad842e51dd0459ce83baa830
Evgeny Novoselov <zkodru@gmail.com>
2022-01-22 10:07:21 +0300

    Добавил модификаторы доступа

--- chapter_005/src/main/java/ru/job4j/productstorage/storage/Warehouse.java
+++ chapter_005/src/main/java/ru/job4j/productstorage/storage/Warehouse.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Warehouse implements Storage {
-    List<Food> foods = new ArrayList<>();
+    private final List<Food> foods = new ArrayList<>();
 
     @Override
     public void add(Food food) {
