7b32724172549bb9f580768e1e0c0bd4d8ead7fa
Evgeny Novoselov <zkodru@gmail.com>
2022-01-22 10:43:54 +0300

    Добавил комментарии к интерфейсу хранилищь

--- chapter_005/src/main/java/ru/job4j/productstorage/storage/Storage.java
+++ chapter_005/src/main/java/ru/job4j/productstorage/storage/Storage.java
@@ -4,10 +4,37 @@ import ru.job4j.productstorage.food.Food;
 
 import java.util.List;
 
+/**
+ * Интерыейс хранилища.
+ */
 public interface Storage {
+    /**
+     * Метод проверяет можно ли положить продукт в хранилище.
+     *
+     * @param food Продукт.
+     * @return Можно ли положить в хранилище.
+     */
+    boolean accept(Food food);
+
+    /**
+     * Добавляет продукт в хранилище.
+     *
+     * @param food Продукт.
+     */
     void add(Food food);
 
+    /**
+     * Удаляет продукт из хранилища.
+     *
+     * @param food Продукт.
+     * @return Получилось ли удалить продукт.
+     */
     boolean remove(Food food);
 
+    /**
+     * Возвращает продукты в хранилище.
+     *
+     * @return Список продуктов в хранилище.
+     */
     List<Food> getFoods();
 }
