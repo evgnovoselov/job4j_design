bb0e1ad0aad63d82274e66614b837e0dfe037acb
Evgeny Novoselov <zkodru@gmail.com>
2022-01-24 15:34:27 +0300

    Сделал возвращение копий машин при запросе всех машин.

--- chapter_005/src/main/java/ru/job4j/parkingcars/car/PassengerCar.java
+++ chapter_005/src/main/java/ru/job4j/parkingcars/car/PassengerCar.java
@@ -1,8 +1,37 @@
 package ru.job4j.parkingcars.car;
 
+import java.util.Objects;
+
 public class PassengerCar implements Car {
+    private static int count = 1;
+    private int id = count++;
+
     @Override
     public int getSize() {
         return 1;
+    }
+
+    @Override
+    public PassengerCar getCopy() {
+        PassengerCar copy = new PassengerCar();
+        copy.id = this.id;
+        return copy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PassengerCar that = (PassengerCar) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
