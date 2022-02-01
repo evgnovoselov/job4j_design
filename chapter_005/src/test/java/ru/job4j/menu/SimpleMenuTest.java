eb6ebf0c284ad570b89e84f3232423f8ca2ffe06
Evgeny Novoselov <zkodru@gmail.com>
2022-02-01 12:10:56 +0300

    Добавил тест

--- chapter_005/src/test/java/ru/job4j/menu/SimpleMenuTest.java
+++ chapter_005/src/test/java/ru/job4j/menu/SimpleMenuTest.java
@@ -2,31 +2,48 @@ package ru.job4j.menu;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class SimpleMenuTest {
+    public static final ActionDelegate STUB_ACTION = System.out::println;
+
     @Test
-    public void whenAddSectionsInMenuAndPrintThenShowMenu() {
-        SimpleMenu menu = new SimpleMenu();
-        MenuElement element1 = new MenuElement("Задача 1.");
-        MenuElement element2 = new MenuElement("Задача 1.1.");
-        MenuElement element3 = new MenuElement("Задача 1.1.1.");
-        MenuElement element4 = new MenuElement("Задача 1.1.2.");
-        MenuElement element5 = new MenuElement("Задача 1.2.");
-        MenuElement element6 = new MenuElement("Задача 2.");
-        element1.add(element2);
-        element2.add(element3);
-        element2.add(element4);
-        element1.add(element5);
-        menu.add(element1);
-        menu.add(element6);
-        String expected = "Menu:" + System.lineSeparator()
-                + "Задача 1." + System.lineSeparator()
-                + "---- Задача 1.1." + System.lineSeparator()
-                + "-------- Задача 1.1.1." + System.lineSeparator()
-                + "-------- Задача 1.1.2." + System.lineSeparator()
-                + "---- Задача 1.2." + System.lineSeparator()
-                + "Задача 2." + System.lineSeparator();
-        assertEquals(expected, menu.toString());
+    public void whenAddThenReturnSame() {
+        Menu menu = new SimpleMenu();
+        menu.add(Menu.ROOT, "Сходить в магазин", STUB_ACTION);
+        menu.add(Menu.ROOT, "Покормить собаку", STUB_ACTION);
+        menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
+        menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
+        menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
+        assertEquals(
+                new Menu.MenuItemInfo(
+                        "Сходить в магазин",
+                        List.of("Купить продукты"),
+                        STUB_ACTION,
+                        "1."
+                ),
+                menu.select("Сходить в магазин").orElseThrow()
+        );
+        assertEquals(
+                new Menu.MenuItemInfo(
+                        "Купить продукты",
+                        List.of("Купить хлеб", "Купить молоко"),
+                        STUB_ACTION,
+                        "1.1."
+                ),
+                menu.select("Купить продукты").orElseThrow()
+        );
+        assertEquals(
+                new Menu.MenuItemInfo(
+                        "Покормить собаку",
+                        List.of(),
+                        STUB_ACTION,
+                        "2."
+                ),
+                menu.select("Покормить собаку").orElseThrow()
+        );
+        menu.forEach(menuItemInfo -> System.out.println(menuItemInfo.getNumber() + menuItemInfo.getName()));
     }
 }
