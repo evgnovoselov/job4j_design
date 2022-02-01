c235f44a1b290cccc568e438f59e25977f3bf468
Evgeny Novoselov <zkodru@gmail.com>
2022-02-01 16:19:04 +0300

    Подготовил вывод

--- chapter_005/src/main/java/ru/job4j/menu/LineMenuPrinter.java
+++ chapter_005/src/main/java/ru/job4j/menu/LineMenuPrinter.java
@@ -1,17 +1,13 @@
 package ru.job4j.menu;
 
-import java.util.Iterator;
-
 /**
  * Реализация принтера, который добавляет черточки и отступы для подразделов.
  */
 public class LineMenuPrinter implements MenuPrinter {
     @Override
     public void print(Menu menu) {
-        Iterator<Menu.MenuItemInfo> iterator = menu.iterator();
-        while (iterator.hasNext()){
-            Menu.MenuItemInfo next = iterator.next();
-            System.out.println(next.getName());
-        }
+        menu.forEach(menuItemInfo -> {
+            System.out.printf("%s %s%n", menuItemInfo.getNumber(), menuItemInfo.getName());
+        });
     }
 }
