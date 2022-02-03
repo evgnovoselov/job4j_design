7eba9a206327d20e96be52f4d0f4f331363fb45e
Evgeny Novoselov <zkodru@gmail.com>
2022-02-03 10:17:39 +0300

    Написал тест вывода меню в консоле с черточками

--- chapter_005/src/test/java/ru/job4j/menu/SimpleMenuTest.java
+++ chapter_005/src/test/java/ru/job4j/menu/SimpleMenuTest.java
@@ -2,6 +2,8 @@ package ru.job4j.menu;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -49,13 +51,23 @@ public class SimpleMenuTest {
 
     @Test
     public void whenAddAndPrintThenPrintSame() {
-        MenuPrinter printer = new LineMenuPrinter();
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream oldOut = System.out;
+        System.setOut(new PrintStream(mem));
         Menu menu = new SimpleMenu();
         menu.add(Menu.ROOT, "Сходить в магазин", STUB_ACTION);
         menu.add(Menu.ROOT, "Покормить собаку", STUB_ACTION);
         menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
         menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
         menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
+        MenuPrinter printer = new LineMenuConsolePrinter();
         printer.print(menu);
+        String expected = "Сходить в магазин" + System.lineSeparator() +
+                "---- Купить продукты" + System.lineSeparator() +
+                "-------- Купить хлеб" + System.lineSeparator() +
+                "-------- Купить молоко" + System.lineSeparator() +
+                "Покормить собаку" + System.lineSeparator();
+        assertEquals(expected, mem.toString());
+        System.setOut(oldOut);
     }
 }
