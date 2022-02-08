7b9b3556e02afa4fcf29d0568568019d9e09f177
Evgeny Novoselov <zkodru@gmail.com>
2022-02-08 23:12:10 +0300

    Сделал печать панели

--- chapter_005/src/main/java/ru/job4j/menu/TODOApp.java
+++ chapter_005/src/main/java/ru/job4j/menu/TODOApp.java
@@ -1,6 +1,25 @@
 package ru.job4j.menu;
 
+import java.util.Scanner;
+
 public class TODOApp {
     public static void main(String[] args) {
+        Menu menu = new SimpleMenu();
+        Scanner scanner = new Scanner(System.in);
+        String action;
+        do {
+            showControl();
+            action = scanner.nextLine();
+        } while (!action.equals("0"));
+    }
+
+    private static void showControl() {
+        System.out.printf("%s Control Panel %1$s%n", "=".repeat(10));
+        System.out.printf("| %-31s |%n", "1. Вывести меню");
+        System.out.printf("| %-31s |%n", "2. Создать пункт меню");
+        System.out.printf("| %-31s |%n", "3. Вызов действия по названию");
+        System.out.printf("| %-31s |%n", "0. Выйти из программы");
+        System.out.println("=".repeat(35));
+        System.out.print("Выберите действие: ");
     }
 }
