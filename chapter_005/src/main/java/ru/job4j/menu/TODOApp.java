e61a93966d53e64bac5bb73c286c5f9a51d14dac
Evgeny Novoselov <zkodru@gmail.com>
2022-02-08 22:20:17 +0300

    Убрал полностью реализацию у класса туду

--- chapter_005/src/main/java/ru/job4j/menu/TODOApp.java
+++ chapter_005/src/main/java/ru/job4j/menu/TODOApp.java
@@ -1,22 +1,6 @@
 package ru.job4j.menu;
 
 public class TODOApp {
-    private final Menu tasks;
-
-    public TODOApp() {
-        this(new SimpleMenu());
-    }
-
-    public TODOApp(Menu tasks) {
-        this.tasks = tasks;
-    }
-
-    public boolean add(String parentName, String childName) {
-        return tasks.add(parentName, childName, () -> {
-        });
-    }
-
-    public Menu getTasks() {
-        return tasks;
+    public static void main(String[] args) {
     }
 }
