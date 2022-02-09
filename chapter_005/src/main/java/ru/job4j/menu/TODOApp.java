15ce991e07f421dbb4c4d850d4c7b350ebd1cc8b
Evgeny Novoselov <zkodru@gmail.com>
2022-02-09 13:31:39 +0300

    1. Создать меню. [#4748 #259867] fix (#132)

--- chapter_005/src/main/java/ru/job4j/menu/TODOApp.java
+++ chapter_005/src/main/java/ru/job4j/menu/TODOApp.java
@@ -1,29 +1,100 @@
 package ru.job4j.menu;
 
-import java.util.Iterator;
+import java.util.Optional;
+import java.util.Scanner;
 
-public class TODOApp implements Iterable<Menu.MenuItemInfo> {
-    private final Menu tasks;
+public class TODOApp {
+    private static final String CONSOLE_SPLIT_LINE = "=".repeat(45);
 
-    public TODOApp() {
-        this(new SimpleMenu());
+    public static void main(String[] args) {
+        Menu tasks = new SimpleMenu();
+        Scanner scanner = new Scanner(System.in);
+        int action;
+        do {
+            showControlPanel();
+            action = getAction(scanner);
+            if (action == 1) {
+                showTasks(tasks);
+            }
+            if (action == 2) {
+                addTaskTo(tasks, scanner);
+            }
+            if (action == 3) {
+                selectDoAction(tasks, scanner);
+            }
+        } while (action != 0);
     }
 
-    public TODOApp(Menu tasks) {
-        this.tasks = tasks;
+    private static void selectDoAction(Menu tasks, Scanner scanner) {
+        System.out.println(CONSOLE_SPLIT_LINE);
+        System.out.print("Введите название задачи, для запуска действия: ");
+        String taskName = scanner.nextLine();
+        Optional<Menu.MenuItemInfo> itemInfo = tasks.select(taskName);
+        if (itemInfo.isEmpty()) {
+            System.out.println("Данной задачи не существует");
+        } else {
+            itemInfo.get().getActionDelegate().delegate();
+        }
+        System.out.println(CONSOLE_SPLIT_LINE);
     }
 
-    public boolean add(String parentName, String childName) {
-        return tasks.add(parentName, childName, () -> {
-        });
+    private static int getAction(Scanner scanner) {
+        String number = scanner.nextLine();
+        return number.isBlank() ? -1 : Integer.parseInt(number);
     }
 
-    public Menu getTasks() {
-        return tasks;
+    private static void showTasks(Menu tasks) {
+        System.out.println(CONSOLE_SPLIT_LINE);
+        System.out.println("Вывод всех задач");
+        new LineMenuConsolePrinter().print(tasks);
+        System.out.println(CONSOLE_SPLIT_LINE);
     }
 
-    @Override
-    public Iterator<Menu.MenuItemInfo> iterator() {
-        return tasks.iterator();
+    private static void addTaskTo(Menu tasks, Scanner scanner) {
+        boolean addTask = false;
+        System.out.println(CONSOLE_SPLIT_LINE);
+        System.out.println("Добавление новой задачи");
+        System.out.print("Введите имя задачи: ");
+        String taskName = scanner.nextLine();
+        System.out.print("Введите имя родительской задачи: ");
+        String parentTaskName = scanner.nextLine();
+        if (parentTaskName.isBlank()) {
+            parentTaskName = Menu.ROOT;
+        }
+        if (!taskName.isBlank()) {
+            ActionDelegate actionDelegate = new ConsoleOutAction(scanner);
+            addTask = tasks.add(parentTaskName, taskName, actionDelegate);
+        }
+        if (addTask) {
+            System.out.println("Задача успешно создана.");
+        }
+        if (taskName.isBlank() || !addTask) {
+            System.out.println("Не удалось добавить задачу!");
+        }
+        System.out.println(CONSOLE_SPLIT_LINE);
+    }
+
+    private static void showControlPanel() {
+        System.out.printf("%s Control Panel %1$s%n", "=".repeat(15));
+        System.out.printf("| %-41s |%n", "1. Вывести задачи");
+        System.out.printf("| %-41s |%n", "2. Создать задачу");
+        System.out.printf("| %-41s |%n", "3. Вызов действия по названию задачи");
+        System.out.printf("| %-41s |%n", "0. Выйти из программы");
+        System.out.println(CONSOLE_SPLIT_LINE);
+        System.out.print("Выберите действие: ");
+    }
+
+    private static class ConsoleOutAction implements ActionDelegate {
+        private final String text;
+
+        public ConsoleOutAction(Scanner scanner) {
+            System.out.print("Введите, что вывести при выборе задачи: ");
+            text = scanner.nextLine();
+        }
+
+        @Override
+        public void delegate() {
+            System.out.println(text);
+        }
     }
 }
