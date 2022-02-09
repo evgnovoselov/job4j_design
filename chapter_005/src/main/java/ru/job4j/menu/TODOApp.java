6e01aa162ad1305e68e68c1dc23d15ba83cd57a0
Evgeny Novoselov <zkodru@gmail.com>
2022-02-09 11:56:37 +0300

    Поправил работу TODOapp

--- chapter_005/src/main/java/ru/job4j/menu/TODOApp.java
+++ chapter_005/src/main/java/ru/job4j/menu/TODOApp.java
@@ -3,23 +3,63 @@ package ru.job4j.menu;
 import java.util.Scanner;
 
 public class TODOApp {
+    private static final String CONSOLE_SPLIT_LINE = "=".repeat(45);
+
     public static void main(String[] args) {
-        Menu menu = new SimpleMenu();
+        Menu tasks = new SimpleMenu();
         Scanner scanner = new Scanner(System.in);
-        String action;
+        int action;
         do {
-            showControl();
-            action = scanner.nextLine();
-        } while (!action.equals("0"));
+            showControlPanel();
+            action = getAction(scanner);
+            if (action == 1) {
+                showTasks(tasks);
+            }
+            if (action == 2) {
+                addTaskTo(tasks, scanner);
+            }
+        } while (action != 0);
     }
 
-    private static void showControl() {
-        System.out.printf("%s Control Panel %1$s%n", "=".repeat(10));
-        System.out.printf("| %-31s |%n", "1. Вывести меню");
-        System.out.printf("| %-31s |%n", "2. Создать пункт меню");
-        System.out.printf("| %-31s |%n", "3. Вызов действия по названию");
-        System.out.printf("| %-31s |%n", "0. Выйти из программы");
-        System.out.println("=".repeat(35));
+    private static int getAction(Scanner scanner) {
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private static void showTasks(Menu tasks) {
+        System.out.println(CONSOLE_SPLIT_LINE);
+        System.out.println("Вывод всех задач");
+        new LineMenuConsolePrinter().print(tasks);
+        System.out.println(CONSOLE_SPLIT_LINE);
+    }
+
+    /**
+     * TODO Выбор действия для задачи.
+     */
+    private static void addTaskTo(Menu tasks, Scanner scanner) {
+        System.out.println(CONSOLE_SPLIT_LINE);
+        System.out.println("Добавление новой задачи");
+        System.out.print("Введите имя родительской задачи: ");
+        String parentTaskName = scanner.nextLine();
+        System.out.print("Введите имя задачи: ");
+        String taskName = scanner.nextLine();
+        if (parentTaskName.isBlank()) {
+            parentTaskName = Menu.ROOT;
+        }
+        if (tasks.add(parentTaskName, taskName, System.out::println)) {
+            System.out.println("Задача успешно создана.");
+        } else {
+            System.out.println("Не удалось добавить задачу!");
+        }
+        System.out.println(CONSOLE_SPLIT_LINE);
+    }
+
+    private static void showControlPanel() {
+        System.out.printf("%s Control Panel %1$s%n", "=".repeat(15));
+        System.out.printf("| %-41s |%n", "1. Вывести задачи");
+        System.out.printf("| %-41s |%n", "2. Создать задачу");
+        System.out.printf("| %-41s |%n", "3. Вызов действия по названию задачи");
+        System.out.printf("| %-41s |%n", "0. Выйти из программы");
+        System.out.println(CONSOLE_SPLIT_LINE);
         System.out.print("Выберите действие: ");
     }
 }
